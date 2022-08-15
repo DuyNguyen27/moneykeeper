1258548aa5d17027d1759ce16355eca0fe37c324
Duy Nguyen <duynk198@gmail.com>
2022-08-15 23:40:18 +0700

    (wp) feat : update auth screen

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -3,21 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutes } from './app.routes';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { AuthComponent } from './components/auth/auth.component';
+import { AuthComponent } from './pages/auth/auth.component';
 import { TranslocoRootModule } from './transloco-root.module';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NxWelcomeComponent,
     AuthComponent,
+    LoginComponent,
+    SignupComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, RouterModule.forRoot(AppRoutes), TranslocoRootModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(AppRoutes),
+    TranslocoRootModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule,
+    DividerModule,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/angular/src/app/pages/auth/auth.component.ts
+++ apps/angular/src/app/pages/auth/auth.component.ts
@@ -1,29 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'angular-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AuthComponent implements OnInit, OnDestroy {
-  constructor() {}
-
-  public isLogging: boolean = true;
+export class AuthComponent implements OnDestroy {
+  public isLogging = true;
   public timeoutFn!: any;
-
-  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     clearTimeout(this.timeoutFn);
   }
 
   public onHandleChangeForm(): void {
+    this.isLogging = !this.isLogging;
     clearTimeout(this.timeoutFn);
     const switchCtn = document.querySelector('#switch-cnt');
     switchCtn?.classList.add('is-gx');
     this.timeoutFn = setTimeout(function () {
       switchCtn?.classList.remove('is-gx');
     }, 1500);
-    this.isLogging = !this.isLogging;
   }
 }
